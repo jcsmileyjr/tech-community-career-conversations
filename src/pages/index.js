15204ba25a33db58f2ed79749aadd5f1ca40f83d
Smiley <jcsmileyjr@gmail.com>
2023-03-21 18:40:42 -0500

    Style changes to add intro text

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,5 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
 import DiamondImage from '../assets/diamond-icon-blue.png'
 import PreviousArrow from '../assets/PolygonLeft.png'
@@ -19,6 +18,24 @@ export default function Home() {
         setContent(data);
       })
   }, []);
+
+  // When the user clicks the Next button, the next story is displayed
+  const nextArticle = () => {
+    if((content.length -1) === (currentArticle)){
+      setCurrentArticle(0);
+    }else{
+      setCurrentArticle(currentArticle + 1);
+    }
+  }
+
+  // When the user clicks the Previous button, the story before the current is displayed
+  const previousArticle = () => {
+    if(currentArticle === 0){
+      setCurrentArticle(content.length - 1);
+    }else{
+      setCurrentArticle(currentArticle - 1);
+    }
+  }
   
   return (
     <>
@@ -35,13 +52,17 @@ export default function Home() {
               <h1 className={`${styles.primaryTextColorBold} ${styles.pageTitle}`}>{content[currentArticle].title}</h1>
               <section className={styles.section}>
                 {content === 'loading' &&
-                    <p>loading</p>
+                  <p>loading</p>
+                }
+
+                {content !== 'loading' &&
+                  <p className={`${styles.content} ${styles.introText} ${styles.primaryTextColorLight}`}>{content[currentArticle].intro}</p>
                 }
 
                 {content !== 'loading' &&
                   <ul className={styles.content}>
-                  {content[currentArticle].listOfBulletPoints.map((bulletpoint) => (
-                    <div className={`${styles.bullet}`}>
+                  {content[currentArticle].listOfBulletPoints.map((bulletpoint, index) => (
+                    <div key={index} className={`${styles.bullet}`}>
                       <div>
                         <Image
                           src={DiamondImage}
@@ -66,7 +87,7 @@ export default function Home() {
               </section>
             </div>
             <footer className={styles.footer}>
-              <div className={styles.footerDirections}>
+              <div className={styles.footerDirections} onClick={() => previousArticle()}>
                 <Image 
                   src={PreviousArrow} 
                   alt="Left Arrow"
@@ -77,7 +98,7 @@ export default function Home() {
                 />
                 <p className={`${styles.footerArrowTitle} ${styles.primaryTextColorBold}`}>Previous</p>
               </div>
-              <div className={styles.footerDirections}>
+              <div className={styles.footerDirections} onClick={() => nextArticle()}>
                 <Image 
                   src={NextArrow} 
                   alt="Right Arrow"
